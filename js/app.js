ecebee9cc9031c36b1939a41ca910c27b1674798
loverobot <1875611803@qq.com>
2017-10-31 17:47:33 +0800

    修改信息

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
 /**
  * Created by yuanshijie on 2017/2/24.
  */
-var app = angular.module('app',['ngRoute']);
-app.config(['$routeProvider',function($routeProvider){
-    $routeProvider.when('/',{})
-        .when('/about',{templateUrl:'about.html'})
-        .otherwise({redirectTo:'/'});
-}]);
